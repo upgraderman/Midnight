abdabed624d49249f43112db7bd837926976ba2d
upgraderman <lootboy10kpiece@gmail.com>
2026-06-27 16:35:07 +0200

    Update opts.h

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://192.168.1.69:8080";
+constexpr const char *BACKEND_URL = "http://209.25.140.26:1499";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
